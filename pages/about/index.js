78b8ebea8e15d366296c3d5a828dd8cfe91e6dd7
PoojaDL <poojadl2002@gmail.com>
2023-09-27 19:36:01 +0530

    added navbar to maintain dynamic routes

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,23 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 const AboutPage = () => {
-  return <h1>This is AboutUS Page</h1>;
+  return (
+    <Fragment>
+      <h1>This is AboutUS Page</h1>
+      <ul>
+        <li>
+          <Link href="/about/1">Yash</Link>
+        </li>
+        <li>
+          <Link href="/about/2">Vaibhav</Link>
+        </li>
+        <li>
+          <Link href="/about/3">Suresh</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default AboutPage;
